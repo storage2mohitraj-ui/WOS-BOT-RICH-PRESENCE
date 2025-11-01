64d5a2c48aafb3b040de2526bb9bb7b7b6c99aa6
Magnus-zzz <yourbook444362@gmail.com>
2025-11-01 23:07:03 +0530

    env update

--- index.js
+++ index.js
@@ -32,3 +32,8 @@ client.once("ready", () => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+// --- Keep Alive for Render Free Plan ---
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Bot is running ✅"));
+app.listen(process.env.PORT || 3000, () => console.log("Keep-alive server started"));
